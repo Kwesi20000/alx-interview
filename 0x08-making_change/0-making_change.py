6df28cc6c2be598bdc4562e962435a57c4c5785a
Kwesi20000 <hendrickskumi@yahoo.com>
2024-06-20 11:15:38 -0700

    hendy

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+A making a change interview question
+"""
+
+
+def makeChange(coins, total):
+    """
+    A function for making changes to an algorithm
+    """
+    if total <= 0:
+        return 0
+
+    min_coins = [float('inf')] * (total + 1)
+    min_coins[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
+
+    return min_coins[total] if min_coins[total] != float('inf') else -1
